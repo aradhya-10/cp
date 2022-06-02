88005c4c8041d33bb4a920d45bc4f4f7e58161fc
Aradhya <202051034@iiitvadodara.ac.in>
2022-06-02 17:57:49 +0530

    Add Matrix Chain Multiplication

---
+++ dp/mcm/mcm_recursive.cpp
@@ -0,0 +1,44 @@
+// FORMAT
+// int solve( int s[], int i, int j)
+// {
+//     if(i>j)
+//         return 0;
+
+//     for(int k=i; k<j; k++)
+//     {
+//         int temp = solve(i,k).f(solve(k+1,j));
+//         ans = f'(temp);
+//     }
+//     return ans;
+// }
+
+// (a x b) * (b x c) => a x c
+// Cost = a*b*c 
+
+// array[] = 40 30 20 10 40
+// Dimension od Matrix Ai = arr[i-1] x arr[i]
+// A1 = 40 x 3  (First matrix )
+// A0 DNE, first matrix is A1
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int solve(int a[], int i, int j)
+{
+    int ans = INT_MAX;
+    if(i>=j)        // = beacuse 1 indices involved hain
+        return 0;
+
+    for(int k=i; k<j; k++) 
+    {
+        int temp = solve(a, i ,k) + solve(a, k+1, j) + a[i]*a[k]*a[j];
+        ans = min(ans, temp);
+    }
+    return ans; 
+} 
+
+int main()
+{
+    int a[] = {10, 30, 5, 60};
+    cout<<solve(a,0,3);
+}
