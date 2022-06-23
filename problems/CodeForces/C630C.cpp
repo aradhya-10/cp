d207875291c10f22d22af284b3f8cd447af801e9
Aradhya <202051034@iiitvadodara.ac.in>
2022-06-23 22:31:55 +0530

    Add files

---
+++ problems/CodeForces/C630C.cpp
@@ -0,0 +1,41 @@
+//LUCKY NUMBERS
+//https://codeforces.com/contest/630/problem/C
+#include "bits/stdc++.h"
+using namespace std;
+#define max(a, b) ((a < b) ? b : a)
+#define min(a, b) ((a > b) ? b : a)
+#define mod 1e9 + 7
+#define rep(a, c) for (int(a) = 0; (a) < (c); (a)++)
+#define repp(i, b, c) for (int(i) = (b); (i) <= (c); (i)++)
+#define rof(a, b, c) for (int(a) = (b); (a) >= (c); (a)--)
+#define INF 1000000000000000003
+typedef long long int ll;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+#define a first
+#define b second
+#define pb push_back
+#define pop pop_back
+#define mp make_pair
+
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    cout<< (1LL<<34)<<endl;
+    cout << 2*((1<<n)-1) << endl;
+}
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+        solve();
+    return 0;
+}
